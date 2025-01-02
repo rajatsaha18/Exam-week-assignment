87d8322917fccca0d8c4f1862d7ce6cf96e570b7
rajatsaha18 <editorrajatdiu@gmail.com>
2025-01-02 16:47:28 +0600

    add comment

--- oop.php
+++ oop.php
@@ -84,6 +84,7 @@ class Book {
    $member1->borrowBook($book1);
    $member2->borrowBook($book2);
 
+   // show book copies
    echo "Available of Copies of Book1 : '{$book1->getTitle()}' : " . $book1->getAvailableCopies() . "<br>";
    echo "Available of Copies of Book2 : '{$book2->getTitle()}' : " . $book2->getAvailableCopies() . "<br>";
    // return book member1 and member2
